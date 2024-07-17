6c4e2f8dbcdaa72900194d8d2e94df250562f0c7
Gunnar <gunnar.montag@gmx.de>
2024-07-17 14:10:51 +0200

    add POST method to create new entries

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -8,4 +8,14 @@ export default async function handler(request, response) {
     const places = await Places.find();
     return response.status(200).json(places);
   }
+
+  if (request.method === "POST") {
+    try {
+      const placeData = request.body;
+      await Places.create(placeData);
+      return response.status(201).json({ status: "Location created." });
+    } catch (error) {
+      response.status(400).json("Error: cant create", { error: error.message });
+    }
+  }
 }
